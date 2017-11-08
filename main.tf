955c7509824a3a79c8af42e1a018c29b8d5d676b
Julia <jlamenza@gmail.com>
2017-11-07 23:26:46 -0200

    update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_instance" "exemple" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Helloww" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags {
     Name = "terraform-example"
   }
 
 }
-
